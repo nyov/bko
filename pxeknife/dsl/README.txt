775303ad1ce94f5e61f54875d283bb01e64ccb13
Pravin Shinde <pravin@black-perl.(none)>
2009-06-16 00:17:59 +0200

    Added the script which will create the minirt24.gz for dsl also added all the files which were needed to support it.

---
+++ pxeknife/dsl/README.txt
@@ -0,0 +1,3 @@
+You need to execute "generate_minirt24.sh" for generating minirt24.sh
+This script takes the vanila minirt24.sh_orig and does all needed changes,
+which are needed to make it able of booting over HTTP.
---
+++ pxeknife/dsl/generate_minirt24.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+#cleanup the old mess
+echo "Removing the old files"
+rm -rf minitrt24.gz minirt24 mpoint
+
+# create a copy of image to modify
+cp minirt24.gz_orig minirt24.gz
+gunzip minirt24.gz
+
+echo "Mounting the filesystem"
+mkdir mpoint
+mount minirt24 -o loop mpoint/
+
+echo "coping the needed files..."
+cp modules/* mpoint/modules/
+cp httpfs/static/* mpoint/static/
+
+cp httpfs/server/httpfs mpoint/static/
+cp httpfs/server/fusermount mpoint/static/
+cp linuxrc mpoint/
+chmod 0777 mpoint/linuxrc 
+
+echo "creating environment, creating links..."
+cd mpoint/static
+chmod 0777 *
+ln -s reboot halt
+ln -s reboot poweroff
+ln -s ifconfig route
+cd ..
+cd ..
+
+echo "un-mounting the filesystem"
+umount mpoint
+rmdir mpoint
+
+echo "Compressing the filesystem"
+gzip -9 minirt24
+
+echo "Done, you can use minirt24.gz"
+
+
