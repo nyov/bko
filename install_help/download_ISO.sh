32396b4fde2aab47248785d87ec211570c7ccd83
Pravin Shinde <pravin@black-perl.(none)>
2009-07-27 20:05:10 +0200

    small improvment, download_ISO.sh will create "ISO" folder

--- install_help/download_ISO.sh
+++ install_help/download_ISO.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 set -e
 
+mkdir ISO
+cd ISO
+
 wget http://rom.etherboot.org/share/pravin/ISO/Fedora-11-i686-Live.iso
 wget http://rom.etherboot.org/share/pravin/ISO/KNOPPIX_V5.1.1CD-2007-01-04-EN.iso
 wget http://rom.etherboot.org/share/pravin/ISO/debian-live-501-i386-gnome-desktop.iso
