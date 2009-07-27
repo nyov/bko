68c995257518377305a2a9b536710997f4aeb8ca
Pravin Shinde <pravin@black-perl.(none)>
2009-07-27 20:03:28 +0200

    Added help script for installtions these scripts will download the gits and also download the ISO's

---
+++ install_help/download_ISO.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -e
+
+wget http://rom.etherboot.org/share/pravin/ISO/Fedora-11-i686-Live.iso
+wget http://rom.etherboot.org/share/pravin/ISO/KNOPPIX_V5.1.1CD-2007-01-04-EN.iso
+wget http://rom.etherboot.org/share/pravin/ISO/debian-live-501-i386-gnome-desktop.iso
+wget http://rom.etherboot.org/share/pravin/ISO/filesystem.squashfs
+
+
+wget http://rom.etherboot.org/share/pravin/ISO/ubuntu-9.04-desktop-i386.iso
+mkdir U
+cd U
+wget http://rom.etherboot.org/share/pravin/ISO/U/filesystem.squashfs
+cd ..
+echo "Downloading is done"
---
+++ install_help/setup.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+git clone http://git.etherboot.org/scm/people/pravin/gpxe.git
+git clone http://git.etherboot.org/scm/people/pravin/BKO.git
