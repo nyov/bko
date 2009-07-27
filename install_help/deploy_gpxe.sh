147d3f4abf5b55ea790c841801773b140e3f4966
Pravin Shinde <pravin@black-perl.(none)>
2009-07-28 00:58:37 +0200

    fixed the problem of deploy_gpxe.sh

--- install_help/deploy_gpxe.sh
+++ install_help/deploy_gpxe.sh
@@ -23,7 +23,9 @@ perl -p -i -e "$regular_expression" pxeSTATIC.gpxe
 ./custom_make.sh
 echo "removing old gpxe images..."
 cd $BKOPATH
-rm -rf ./gpxe_images/*
+rm -rf ./gpxe_images
+mkdir ./gpxe_images
+
 echo "copying gpxe images..."
 cp $gpxePATH/src/bin/gpxe.dsk ./gpxe_images/
 cp $gpxePATH/src/bin/gpxe.usb ./gpxe_images/
