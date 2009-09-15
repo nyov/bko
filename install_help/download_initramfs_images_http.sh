852cbae72fc347eb513de7a38f7ccb470ccbb79b
Pravin Shinde <pravin@black-perl.(none)>
2009-09-16 00:07:21 +0200

    Fixed the download_initramfs_image_http script

--- install_help/download_initramfs_images_http.sh
+++ install_help/download_initramfs_images_http.sh
@@ -2,42 +2,43 @@
 set -e
 
 # pick the configuration information
-source ../bko/configure
+source ../config
 
 # all scripts are in install folder
 cd ..
 
 REMOTE_BKO_LOCATION=$REMOTE_INITRAMFS_DOWNLOAD_LOCATION
+REMOTE_BKO_LOCATION="http://rom.etherboot.org/share/pravin/BKO/"
 LOCAL_BKO_LOCATION=`pwd`
 
 
 echo "downloading dsl initramfs"
-FILE_TO_COPY=/D/dsl/minirt24.gz
+FILE_TO_COPY=/bko/live/dsl/minirt24.gz
 rm -f $LOCAL_BKO_LOCATION/$FILE_TO_COPY
 wget $REMOTE_BKO_LOCATION/$FILE_TO_COPY -O $LOCAL_BKO_LOCATION/$FILE_TO_COPY
 
 echo "downloading knoppix initramfs"
-FILE_TO_COPY=/D/K/K5/minirt.gz
+FILE_TO_COPY=/bko/live/knoppix/5.11/minirt.gz
 rm -f $LOCAL_BKO_LOCATION/$FILE_TO_COPY
 wget $REMOTE_BKO_LOCATION/$FILE_TO_COPY -O $LOCAL_BKO_LOCATION/$FILE_TO_COPY
 
 echo "downloading knoppix6 initramfs"
-FILE_TO_COPY=/D/K/K6/minirt.gz
+FILE_TO_COPY=/bko/live/knoppix/6.0.1/minirt.gz
 rm -f $LOCAL_BKO_LOCATION/$FILE_TO_COPY
 wget $REMOTE_BKO_LOCATION/$FILE_TO_COPY -O $LOCAL_BKO_LOCATION/$FILE_TO_COPY
 
 echo "downloading fedora initramfs"
-FILE_TO_COPY=/D/R/F/initrd0.img 
+FILE_TO_COPY=/bko/live/fedora/f11/initrd0.img
 rm -f $LOCAL_BKO_LOCATION/$FILE_TO_COPY
 wget $REMOTE_BKO_LOCATION/$FILE_TO_COPY -O $LOCAL_BKO_LOCATION/$FILE_TO_COPY
 
 echo "downloading debian initramfs"
-FILE_TO_COPY=/D/D/DL/initrd1.img
+FILE_TO_COPY=/bko/live/debian/initmod/initrd1.img
 rm -f $LOCAL_BKO_LOCATION/$FILE_TO_COPY
 wget $REMOTE_BKO_LOCATION/$FILE_TO_COPY -O $LOCAL_BKO_LOCATION/$FILE_TO_COPY
 
 echo "downloading ubuntu initramfs"
-FILE_TO_COPY=/D/U/U9/initrd.gz
+FILE_TO_COPY=/bko/live/ubuntu/U9/initrd.gz
 rm -f $LOCAL_BKO_LOCATION/$FILE_TO_COPY
 wget $REMOTE_BKO_LOCATION/$FILE_TO_COPY -O $LOCAL_BKO_LOCATION/$FILE_TO_COPY
 
