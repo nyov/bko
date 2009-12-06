569c44dfa5d6cb29cbf67bad991fe0817adc859e
Pravin Shinde <pravin@black-perl.(none)>
2009-12-06 19:53:02 +0100

    Fixed generate_initrd.sh to work with stable karmic kolaha also fetched the casper from newer initramfs.

--- bko/live/ubuntu/U9.10/generate_initrd.sh
+++ bko/live/ubuntu/U9.10/generate_initrd.sh
@@ -3,10 +3,10 @@ set -e
 
 #cleanup the old mess
 echo "Removing the old files"
-sudo rm -rf initrd.lz initrd mpoint
+sudo rm -rf mpoint initrd initrd.lzma
 
 # create a copy of image to modify
-cp initrd_orig initrd.lzma
+cp initrd_orig.lz initrd.lzma
 unlzma initrd.lzma
 
 echo "extracting filesystem filesystem"
@@ -14,8 +14,6 @@ mkdir -p mpoint
 cd mpoint 
 cpio -ivu --no-absolute-filename  < ../initrd 2> /dev/null
 cd ..
-
-
 
 echo "coping the needed files..."
 
@@ -48,11 +46,9 @@ sudo chown -R root.root mpoint
 cd mpoint
 echo "creating initrd from new system filesystem"
 echo "Compressing the filesystem"
-echo "exiting here as this part is not yet implemented."
-exit 0  # exiting here as I have not worked on compressing back the image.
-sudo find . | sudo cpio -oH newc | gzip -9 > ../initrd.gz
+sudo find . | sudo cpio -oH newc | lzma -9 -c> ../initrd.lz
 cd ..
 
-sudo rm -rf mpoint initrd
+sudo rm -rf mpoint initrd initrd.lzma
 
 echo "Done, you can use initrd.gz"
