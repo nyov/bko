1c6c9a195856601e3ff8afe86f13ccc43db30157
Pravin Shinde <pravin@black-perl.(none)>
2009-12-26 15:04:39 +0100

    Fixed DNS issues with ubuntu-9.10

--- install_help/upload_initramfs_images.sh
+++ install_help/upload_initramfs_images.sh
@@ -6,7 +6,11 @@ echo "uploading debian initramfs"
 FILE_TO_COPY=/bko/live/debian/initmod/initrd1.img
 scp $LOCAL_BKO_LOCATION/$FILE_TO_COPY $REMOTE_BKO_LOCATION/$FILE_TO_COPY
 
-echo "uploading ubuntu initramfs"
+echo "uploading ubuntu-9.10 initramfs"
+FILE_TO_COPY=/bko/live/ubuntu/U9.10/initrd.lz
+scp $LOCAL_BKO_LOCATION/$FILE_TO_COPY $REMOTE_BKO_LOCATION/$FILE_TO_COPY
+
+echo "uploading ubuntu-9.04 initramfs"
 FILE_TO_COPY=/bko/live/ubuntu/U9/initrd.gz
 scp $LOCAL_BKO_LOCATION/$FILE_TO_COPY $REMOTE_BKO_LOCATION/$FILE_TO_COPY
 
